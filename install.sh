f43265648234ccfcba0caff0935092e38e1803be
Yojan Gautam <gautamyojan0@gmail.com>
2022-10-26 20:55:49 -0400

    testing install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/Super-Yojan/drMoscoVIM ~/.drMoscoVIM
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+ln -s ~/.drMoscoVIM/nvim/.config/nvim ~/.config/nvim
+
+
