0608b3e847d3cdbd0f06d78d87b2fab5d8483563
Yojan Gautam <gautamyojan0@gmail.com>
2022-10-26 21:11:15 -0400

    script should be working decently

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ git clone https://github.com/Super-Yojan/drMoscoVIM ~/.drMoscoVIM
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 mkdir -p ~/.config
+rm -rf ~/.config/nvim
 ln -s ~/.drMoscoVIM/nvim/.config/nvim ~/.config/nvim
-
-
+nvim +PackerInstall
